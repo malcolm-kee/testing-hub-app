2907c5f293f30a2511762f60f9d8fcc98665f19c
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-03-28 21:14:00 +0800

    update base template

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export function App() {
+  return (
+    <div className="text-center max-w-xl shadow my-6 px-4 py-2 mx-auto rounded">
+      <header>
+        <div className="text-3xl">React App</div>
+      </header>
+      <main>
+        <p>This application is preconfigured with</p>
+        <ul>
+          <li>TypeScript</li>
+          <li>SASS</li>
+          <li>TailwindCSS</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
